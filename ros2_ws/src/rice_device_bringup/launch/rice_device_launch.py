6a9e29cf2e75f00f89607bd8c89ffd1155b69550
keenan88 <lakingsfan08@gmail.com>
2023-10-31 10:45:02 -0400

    Combined bno055, angle extractor, and rviz launch into one launch file for convenience

---
+++ ros2_ws/src/rice_device_bringup/launch/rice_device_launch.py
@@ -0,0 +1,21 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+ros2_ws_path = '/home/keenan/Documents/rice_device/ros2_ws/'
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='euler_angle_extractor',
+            executable='euler_angle_extractor_exe',
+        ),
+        Node(
+            package='bno055',
+            executable='bno055',
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            arguments=['-d', [ros2_ws_path + 'src/rice_device_bringup/rviz_configs/rviz_bno055_config.rviz']]
+        )
+    ])
--- ros2_ws/src/rice_device_bringup/setup.py
+++ ros2_ws/src/rice_device_bringup/setup.py
@@ -1,6 +1,8 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
-package_name = 'euler_angle_extractor'
+package_name = 'rice_device_bringup'
 
 setup(
     name=package_name,
@@ -10,6 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*')))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,7 +23,6 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'euler_angle_extractor_exe = euler_angle_extractor.euler_angle_extractor:main'
         ],
     },
 )
