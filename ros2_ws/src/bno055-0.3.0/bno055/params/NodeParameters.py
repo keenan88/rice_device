4933d8c365641f097fdf8484eb3a89d11d31ca38
keenan88 <lakingsfan08@gmail.com>
2023-10-31 11:21:26 -0400

    Changed udev rule to target ttyUSB device, instead of generic USB device. This allows the symlink to be used by the BNO055 ROS2 node

--- ros2_ws/src/bno055-0.3.0/bno055/params/NodeParameters.py
+++ ros2_ws/src/bno055-0.3.0/bno055/params/NodeParameters.py
@@ -55,7 +55,7 @@ class NodeParameters:
         # The type of the sensor connection. Either "uart" or "i2c":
         node.declare_parameter(name='connection_type', value=UART.CONNECTIONTYPE_UART)
         # UART port
-        node.declare_parameter('uart_port', value='/dev/ttyUSB0')
+        node.declare_parameter('uart_port', value='/dev/CP2102_USB_UART_Convertor2')
         # UART Baud Rate
         node.declare_parameter('uart_baudrate', value=115200)
         # UART Timeout in seconds
