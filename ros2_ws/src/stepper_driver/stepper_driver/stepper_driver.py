b00c249323a215003c2a4fbdbb2e643defb8e02e
Keenan88 <lakingsfan08@gmail.com>
2024-02-26 19:24:51 -0500

    motor spins at target velocities

--- ros2_ws/src/stepper_driver/stepper_driver/stepper_driver.py
+++ ros2_ws/src/stepper_driver/stepper_driver/stepper_driver.py
@@ -1,8 +1,10 @@
 import rclpy
 from rclpy.node import Node
-
 from gpiozero import DigitalOutputDevice
 from time import sleep
+from std_msgs.msg import String
+
+
 
 usleep = lambda x: sleep(x/1000000.0)
 
@@ -12,29 +14,38 @@ class StepperDriver(Node):
     def __init__(self):
         super().__init__('AccelExtractor')
 
-        timer_period_s = 0.01
-
-        self.vel_subscriber = self.create_subscription('/motor_speed', self.set_speed_cb, 10)
-
-        self.step_size = 0.5
-        self.degrees_per_step = 1.8
-
-        self.turn_steppers = self.create_timer(timer_period_s, self.step_callback) 
-
+        self.asdf = self.create_subscription(String, '/motor_speed', self.set_speed_cb, 10)
+        
+        self.stepper_timer = self.create_timer(1, self.step_callback) 
+        self.set_motor_degrees_per_s(30)
+        
         self.step_pin = DigitalOutputDevice(pin = 17, active_high=True, initial_value=False)
-
-        self.i = 0
 
         usleep(2000) # Must sleep atleast 1ms on startup, page 8 of drv8834 datasheet
 
-    def step_callback(self):
-        self.i += 1
-        print(self.i)  
+        print("period (ns):", self.stepper_timer.timer_period_ns)
 
+    def set_motor_degrees_per_s(self, degrees_per_s):
+        degrees_per_full_step = 1.8
+        step_size = 1/2
+        degrees_per_pulse = degrees_per_full_step * step_size
+        pulses_per_s = degrees_per_s / degrees_per_pulse
+        pulse_period_ns = 10e8 / pulses_per_s
+
+        self.stepper_timer.timer_period_ns = pulse_period_ns
+        
+        return pulse_period_ns
+
+    def step_callback(self):
+        
         self.step_pin.on()
         usleep(20) # Minimum 1.9us high time, as per page 8 of drv8834 datasheet
         self.step_pin.off() 
         usleep(20) # Minimum 1.9us low time, as per page 8 of drv8834 datasheet
+
+    def set_speed_cb(self, msg):
+        self.degrees_per_s = float(msg.data)
+        pass
 
 
 
