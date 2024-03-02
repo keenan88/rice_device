5fe289a7704a4cd7f885835bec92d1ba80a4e4f4
Keenan88 <lakingsfan08@gmail.com>
2024-03-01 19:24:36 -0500

    Wheels are spinning to give target velocities

--- ros2_ws/src/stepper_driver/stepper_driver/stepper_driver.py
+++ ros2_ws/src/stepper_driver/stepper_driver/stepper_driver.py
@@ -15,26 +15,42 @@ class StepperDriver(Node):
     def __init__(self):
         super().__init__('Stepper_Driver')
 
-        self.asdf = self.create_subscription(Twist, '/cmd_vel', self.set_speed_cb, 10)
+        self.asdf = self.create_subscription(Twist, '/cmd_vel', self.set_motors_degrees_per_s, 10)
         
         self.right_step_timer = self.create_timer(1, self.right_step_callback) 
         self.left_step_timer = self.create_timer(1, self.left_step_callback) 
 
-        starting_velocity = Twist()
-        self.set_motor_degrees_per_s(starting_velocity)
-        
+        self.left_stopped = False
+        self.right_stopped = False
+
         self.right_step_pin = DigitalOutputDevice(pin = 17, active_high=True, initial_value=False)
         self.right_dir_pin = DigitalOutputDevice(pin = 27, active_high=True, initial_value=False)
 
-        self.left_step_pin = DigitalOutputDevice(pin = 14, active_high=True, initial_value=False)
-        self.left_dir_pin = DigitalOutputDevice(pin = 15, active_high=True, initial_value=False)
+        self.left_step_pin = DigitalOutputDevice(pin = 23, active_high=True, initial_value=False)
+        self.left_dir_pin = DigitalOutputDevice(pin = 24, active_high=True, initial_value=False)
 
+        starting_velocity = Twist()
+        self.set_motors_degrees_per_s(starting_velocity)
+    
         usleep(2000) # Must sleep atleast 1ms on startup, page 8 of drv8834 datasheet
 
-    def set_motor_degrees_per_s(self, twist_msg: Twist):
+    def set_motors_degrees_per_s(self, twist_msg: Twist):
 
         wheel_horiz_offset_m = 0.1 # Need to validate this
         wheel_radius_m = 101.8/1000/2
+
+        lin_max_meters_per_s = 0.15
+        ang_max_rad_per_s = pi / 2
+
+        if twist_msg.linear.x > lin_max_meters_per_s:
+            twist_msg.linear.x = lin_max_meters_per_s
+        if twist_msg.linear.x < -lin_max_meters_per_s:
+            twist_msg.linear.x = -lin_max_meters_per_s
+
+        if twist_msg.angular.z > ang_max_rad_per_s:
+            twist_msg.angular.z = ang_max_rad_per_s
+        if twist_msg.angular.z < -ang_max_rad_per_s:
+            twist_msg.angular.z = -ang_max_rad_per_s
 
         linear_component_rad_per_s = twist_msg.linear.x / wheel_radius_m
         rotation_component_rad_per_s = twist_msg.angular.z * wheel_horiz_offset_m / wheel_radius_m 
@@ -45,26 +61,65 @@ class StepperDriver(Node):
         right_wheel_deg_per_s = right_wheel_rad_per_s * 180 / pi
         left_wheel_deg_per_s = left_wheel_rad_per_s * 180 / pi
 
+        print("R deg/s:", round(right_wheel_deg_per_s, 2))
+        print("L deg/s:", round(left_wheel_deg_per_s, 2))
+        print()
+
         degrees_per_full_step = 1.8
         step_size = 1/2
         degrees_per_pulse = degrees_per_full_step * step_size
 
         right_pulses_per_s = right_wheel_deg_per_s / degrees_per_pulse
-        right_pulse_period_ns = 10e8 / right_pulses_per_s
+        print(right_pulses_per_s)
+
+        if right_pulses_per_s != 0:
+            self.right_stopped = False
+            right_pulse_period_ns = 10e8 / abs(right_pulses_per_s)
+            self.right_step_timer.timer_period_ns = right_pulse_period_ns
+            
+        else:
+            self.right_stopped = True
 
         left_pulses_per_s = left_wheel_deg_per_s / degrees_per_pulse
-        left_pulse_period_ns = 10e8 / left_pulses_per_s
 
-        self.right_stepper_timer.timer_period_ns = right_pulse_period_ns
-        self.left_stepper_timer.timer_period_ns = left_pulse_period_ns
+        if left_pulses_per_s != 0:
+            left_pulse_period_ns = 10e8 / abs(left_pulses_per_s)
+            self.left_step_timer.timer_period_ns = left_pulse_period_ns
+            self.left_stopped = False
+        else:
+            self.left_stopped = True
 
-
-    def step_callback(self):
+        if right_wheel_deg_per_s > 0:
+            self.right_dir_pin.on()
+            print("Dir pin off")
+        else:
+            self.right_dir_pin.off()
+            print("Dir pin on")
         
-        self.right_step_pin.on()
-        usleep(20) # Minimum 1.9us high time, as per page 8 of drv8834 datasheet
-        self.right_step_pin.off() 
-        usleep(20) # Minimum 1.9us low time, as per page 8 of drv8834 datasheet
+        if left_wheel_deg_per_s > 0:
+            self.left_dir_pin.on()
+        else:
+            self.left_dir_pin.off()
+
+        
+
+
+    def right_step_callback(self):
+
+        if not self.right_stopped:        
+            self.right_step_pin.on()
+            usleep(20) # Minimum 1.9us high time, as per page 8 of drv8834 datasheet
+            self.right_step_pin.off() 
+            usleep(20) # Minimum 1.9us low time, as per page 8 of drv8834 datasheet
+
+    def left_step_callback(self):
+        
+        if not self.left_stopped:
+
+            self.left_step_pin.on()
+            usleep(20) # Minimum 1.9us high time, as per page 8 of drv8834 datasheet
+            self.left_step_pin.off() 
+            usleep(20) # Minimum 1.9us low time, as per page 8 of drv8834 datasheet
 
         
 
