c25ffd09bdcfba88b111b40fec72c95f3db1c660
Keenan88 <lakingsfan08@gmail.com>
2024-03-28 20:42:08 -0400

    Built out second state of robot behaviour. Also cleaned up old code in robot behaviour that will be pushed to a lower level

--- ros2_ws/src/robot_behaviour/robot_behaviour/robot_behaviour.py
+++ ros2_ws/src/robot_behaviour/robot_behaviour/robot_behaviour.py
@@ -7,11 +7,9 @@ from std_msgs.msg import Bool, Int32
 from geometry_msgs.msg import Twist
 from li_interface.action import MoveLIAction
 from rclpy.action import ActionClient
-from nav_msgs.msg import Odometry
 
 ROBOT_STATE_INIT_DO_NOTHING = Int32(data = 16)
 ROBOT_STATE_INIT_ALL_UP = Int32(data = 0)
-ROBOT_STATE_INIT_CENTRAL_SHAFT_DOWN = Int32(data = 1)
 ROBOT_STATE_INIT_OUTSIDE_RAILS_DOWN = Int32(data = 2)
 ROBOT_STATE_INIT_CENTRAL_SHAFT_UP = Int32(data = 3)
 ROBOT_STATE_INIT_CENTER_RAIL_DOWN = Int32(data = 4)
@@ -49,9 +47,6 @@ class Robot_Behaviour(Node):
         self.robot_zeroer = self.create_publisher(Int32, '/zero_robot', 10)
 
         # Subscribers to various subsystems feedback
-        self.central_shaft_subscriber = self.create_subscription(
-            Int32, '/central_shaft_status', self.central_shaft_callback, 10
-        )   
 
         self.side_li_action_client = ActionClient(self, MoveLIAction, 'move_side_lis')
 
@@ -64,10 +59,8 @@ class Robot_Behaviour(Node):
             Int32, '/human_input', self.human_input_callback, 10
         )
 
-        self.robot_odom_subscriber = self.create_subscription(
-            Odometry, '/robot_odom', self.robot_odom_callback, 10
-        )
 
+        # States of central shaft and flopper LAs
         self.center_LI_in_motion = False
         self.center_LI_motion_initiated = False
 
@@ -80,7 +73,12 @@ class Robot_Behaviour(Node):
         self.central_shaft_reached_goal = False
         self.human_input_received = False
 
-        self.robot_odom = Odometry()
+        # States of localization
+        self.within_hole_threshold = False
+        self.within_final_hex_transition = False
+        self.within_turn_completion_threshold = False
+
+
 
     # Side Flopper Rail Action Client
     def send_side_LI_goal(self, desired_pos):
@@ -188,12 +186,6 @@ class Robot_Behaviour(Node):
     def human_input_callback(self, msg: Int32):
         self.human_input_received = True
 
-    def central_shaft_callback(self, shaft_reached_goal: Bool):
-        self.central_shaft_reached_goal = shaft_reached_goal.data
-
-    def robot_odom_callback(self, msg: Odometry):
-        self.robot_odom = msg
-
     def update_robot_state(self):
 
         if self.robot_behaviour_state == ROBOT_STATE_INIT_DO_NOTHING:
@@ -221,47 +213,42 @@ class Robot_Behaviour(Node):
                 self.central_shaft_motion_initiated = True
                 self.send_central_shaft_goal("above_wheels")
         
-
-            # Bring central shaft up
-            #shaft_msg = String()
-            #shaft_msg.data = "high"
-            #self.central_shaft_publisher.publish(shaft_msg)
-
-            # Wait until all subsystems confirm complete movement
-            if not self.central_shaft_in_motion:# and not self.center_LI_in_motion:
-                #if self.central_shaft_reached_goal and self.human_input_received:
-                print("DONE")
-                self.side_LI_motion_initiated = False
-                #self.robot_behaviour_state = ROBOT_STATE_INIT_CENTRAL_SHAFT_DOWN
-                while 1: pass
-                self.human_input_received = False
-                self.central_shaft_reached_goal = False
-                self.center_LI_reached_goal = False
-                self.side_LI_reached_goal = False
-
-        elif self.robot_behaviour_state == ROBOT_STATE_INIT_CENTRAL_SHAFT_DOWN:
             
-            shaft_msg = String()
-            shaft_msg.data = "low"
-            self.central_shaft_publisher.publish(shaft_msg)
+            if not self.side_LI_in_motion and not self.center_LI_in_motion:
+                if not self.central_shaft_in_motion:
 
-            if self.human_input_received and self.central_shaft_reached_goal:
-                self.robot_behaviour_state = ROBOT_STATE_INIT_OUTSIDE_RAILS_DOWN
-                self.human_input_received = False
-                self.central_shaft_reached_goal = False
+                    # Reset central shaft and LA state flags
+                    self.side_LI_motion_initiated = False
+                    self.center_LI_motion_initiated = False
+                    self.central_shaft_motion_initiated = False
+                    self.central_shaft_reached_goal = False
+                    self.center_LI_reached_goal = False
+                    self.side_LI_reached_goal = False
 
+                    self.robot_behaviour_state = ROBOT_STATE_INIT_OUTSIDE_RAILS_DOWN
+                    
 
         elif self.robot_behaviour_state == ROBOT_STATE_INIT_OUTSIDE_RAILS_DOWN:
 
-            moveDown = False
-            rail_msg = Bool()
-            rail_msg.data = moveDown
-            self.side_LI_publisher.publish(rail_msg)
+            if not self.side_LI_motion_initiated:
+                self.side_LI_motion_initiated = True
+                self.send_side_LI_goal("down")
 
-            if self.human_input_received and self.side_LI_reached_goal:
-                self.robot_behaviour_state = ROBOT_STATE_INIT_CENTRAL_SHAFT_UP
-                self.human_input_received = False
+            if not self.center_LI_motion_initiated:
+                self.center_LI_motion_initiated = True
+                self.send_center_LI_goal("down")
+
+
+            if self.side_LI_reached_goal and self.center_LI_reached_goal:
+                # Reset LA state flags
+                self.side_LI_motion_initiated = False
+                self.center_LI_motion_initiated = False
+                self.center_LI_reached_goal = False
                 self.side_LI_reached_goal = False
+
+                self.robot_behaviour_state = ROBOT_STATE_INIT_CENTRAL_SHAFT_UP
+                while 1: pass
+                
             
         elif self.robot_behaviour_state == ROBOT_STATE_INIT_CENTRAL_SHAFT_UP:
 
@@ -293,6 +280,7 @@ class Robot_Behaviour(Node):
 
             if self.human_input_received:
                 self.robot_behaviour_state = ROBOT_STATE_FULL_SPEED
+                self.within_hole_threshold = False
                 self.human_input_received = False
 
         elif self.robot_behaviour_state == ROBOT_STATE_FULL_SPEED:
@@ -300,7 +288,7 @@ class Robot_Behaviour(Node):
             speed_msg.data = "full_speed"
             self.drive_publisher.publish(speed_msg)
             
-            if self.robot_odom.pose.pose.position.x > 0.45: # Within 15 cm of pivot hole
+            if self.within_final_hex_transition:
                 self.robot_behaviour_state = ROBOT_STATE_LINEUP_CENTRAL_SHAFT
             
 
@@ -309,7 +297,7 @@ class Robot_Behaviour(Node):
             speed_msg. data = "slow"
             self.drive_publisher.publish(speed_msg) 
             
-            if self.robot_odom.pose.pose.position.x > 0.6 - 2 / 1000: # Within 2mm of pivot hole
+            if self.within_hole_threshold: # Within 2mm of pivot hole
                 self.robot_behaviour_state = ROBOT_STATE_LINEUP_CENTRAL_SHAFT
 
         elif self.robot_behaviour_state == ROBOT_STATE_STOP:
@@ -345,18 +333,20 @@ class Robot_Behaviour(Node):
             self.side_LI_publisher.publish(rail_msg)
 
             if self.side_LI_reached_goal:
+                self.within_turn_completion_threshold = False
                 self.robot_behaviour_state = ROBOT_STATE_TURN_180
                 self.side_LI_reached_goal = False  
 
         elif self.robot_behaviour_state == ROBOT_STATE_TURN_180:
             drive_msg = String()
             drive_msg.data = "turn"
+
             self.drive_publisher.publish(drive_msg)
 
             robot_quat = self.robot_odom.pose.pose.orientation
             robot_euler_orientation = tf.transformations.euler_from_quaternion([robot_quat.x, robot_quat.y, robot_quat.z, robot_quat.w])
 
-            if robot_euler_orientation[2] > 3.14:
+            if self.within_turn_completion_threshold:
                 self.robot_behaviour_state = ROBOT_STATE_EXTEND_SIDE_FLOPPER_RAILS
 
         elif self.robot_behaviour_state == ROBOT_STATE_EXTEND_SIDE_FLOPPER_RAILS:
